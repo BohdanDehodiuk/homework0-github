0ca5fb5d33ea2c2415f5d9df6eae85de13a8e130
BohdanDehodiuk <86683356+BohdanDehodiuk@users.noreply.github.com>
2021-10-16 18:41:31 +0300

    update block 3 2.0

--- third-block.js
+++ third-block.js
@@ -73,7 +73,6 @@
 // apartment.location.city = "Kingston";
 // console.log(apartment.location);
 
-
 // home work 8
 
 // const name = "Repair Droid";
@@ -138,8 +137,6 @@
 //     console.log(values);
 //     }
 
-    
-
 //   // Change code above this line
 // }
 
@@ -160,7 +157,7 @@
 //             console.log(propCount);
 
 //         }
-            
+
 //     }
 
 //   // Change code above this line
@@ -170,3 +167,239 @@
 // countProps({});
 // countProps({ name: "Mango", age: 2 });
 // countProps({ mail: "poly@mail.com", isOnline: true, score: 500 })
+
+// const listOfStudents = ['катя', 'антон', 'оля', 'діма', 'юра', 'костя', 'іра', 'ден', 'міша', 'юля'];
+
+// const newList = listOfStudents.map(name => {
+//     let newListSplit = name.split("");
+//     // console.log(newListSplit);
+//     newListSplit[0] = newListSplit[0].toUpperCase();
+//     // console.log(newListSplit);
+//     newListSplit = newListSplit.join("");
+//     return newListSplit;
+// });
+
+// console.log(listOfStudents);
+// console.log(newList[0]);
+
+// const name = "Kolia";
+// const credit = 123;
+
+// const myCreditCard = {
+//     name,
+//     credit,
+
+//     addToocard(sum) {
+//         this.credit += sum
+//         console.log(`Пользователь ${this.name} добавил на карту ${sum}, баланс по карте ${this.credit}`);
+//     },
+
+//     withdraw(sum) {
+//         this.credit -= sum
+//         console.log(`Пользователь ${this.name} снял с карты ${sum}, баланс по карте ${this.credit}`);
+
+//     }
+
+// }
+// myCreditCard.name = "Masha";
+
+// myCreditCard.addToocard(1000);
+// myCreditCard.withdraw(523);
+
+// const bohdan = {
+//     html: 2,
+//     css: 2,
+//     sass: 1,
+//     js: 2,
+// }
+
+// const values = Object.values(bohdan);
+// console.log(values);
+
+// function addSum(arr) {
+
+//     const tottal = arr.reduce((prewVal, item) => {return prewVal + item}, 0);
+
+//     return tottal;
+// }
+
+// const proffit = addSum(values);
+// console.log(proffit);
+
+// for (const key in bohdan) {
+//     console.log(key);
+//     console.log(bohdan[key]);
+
+// }
+
+// ДЗ від ментора
+// const addSum1 = (...arr) => { console.log(arr) };
+
+// addSum1(values);
+
+// const myCreditCard = {
+//   name: "sergey",
+//   cards: {
+//     privat: 1000,
+//     mono: 2000,
+//     aBank: 3600,
+//   },
+//   total: 100,
+//   sumInMyCard() {
+//     const cardName = prompt();
+//     console.log(cardName);
+//     console.log(this.cards[cardName]);
+//   },
+
+//   addToCard(sum, nameCard) {
+//     this.credits += sum;
+//     console.log(
+//       `Клиент ${this.name} отправил на карту ${sum}. Баланс ${this.credits}`
+//     );
+//   },
+//   withdraw(sum, nameCard) {
+//     this.credits -= sum;
+//     console.log(
+//       `Клиент ${this.name} снял с карты ${sum}. Баланс ${this.credits}`
+//     );
+//   },
+//   addSum() {
+//     const values = Object.values(this.cards);
+
+//     this.total = values.reduce((accum, value) => {
+//       return (accum += value);
+//     }, this.total);
+//     console.log(this.total);
+//   },
+// };
+// myCreditCard.addSum();
+// myCreditCard.addSum();
+
+// home work #13
+
+// const apartment = {
+//   descr: "Spacious apartment in the city center",
+//   rating: 4,
+//   price: 2153,
+// };
+// const values = [];
+// // Change code below this line
+// const keys = Object.keys(apartment);
+// console.log(keys);
+
+// for (const key of keys) {
+
+//     values.push(apartment[key]);
+
+//     // const values = [apartment[key]];
+
+//     console.log(values);
+
+// };
+
+// home work #14
+
+// function countProps(object) {
+//   // Change code below this line
+//     let propCount = 0;
+
+//     const keys = Object.keys(object);
+//     // console.log(keys);
+
+//     propCount = keys.length;
+//     console.log(propCount);
+
+//   return propCount;
+//   // Change code above this line
+// }
+
+// countProps({});
+// countProps({ name: "Mango", age: 2 });
+// countProps({ mail: "poly@mail.com", isOnline: true, score: 500 });
+
+// home work #16
+
+// function countTotalSalary(salaries) {
+//   let totalSalary = 0;
+//   // Change code below this line
+//     const values = Object.values(salaries);
+//     for (const value of values) {
+//         totalSalary += value;
+//         console.log(totalSalary);
+//     }
+
+//   // Change code above this line
+//   return totalSalary;
+// }
+
+// countTotalSalary({});
+// countTotalSalary({ mango: 100, poly: 150, alfred: 80 });
+// countTotalSalary({ kiwi: 200, poly: 50, ajax: 150 });
+
+// home work #17
+
+// const colors = [
+//   { hex: "#f44336", rgb: "244,67,54" },
+//   { hex: "#2196f3", rgb: "33,150,243" },
+//   { hex: "#4caf50", rgb: "76,175,80" },
+//   { hex: "#ffeb3b", rgb: "255,235,59" },
+// ];
+
+// const hexColors = [];
+// const rgbColors = [];
+// // Change code below this line
+// for (const color of colors) {
+//   hexColors.push(color.hex);
+//   rgbColors.push(color.rgb);
+// }
+
+// home work #18
+// const products = [
+//   { name: "Radar", price: 1300, quantity: 4 },
+//   { name: "Scanner", price: 2700, quantity: 3 },
+//   { name: "Droid", price: 400, quantity: 7 },
+//   { name: "Grip", price: 1200, quantity: 9 },
+// ];
+
+// function getProductPrice(productName) {
+//   // Change code below this line
+//   for (const product of products) {
+//     if (product.name === productName) {
+//       // console.log(product.price);
+//       return product.price;
+//     }
+//   }
+//   return null;
+//   // Change code above this line
+// }
+
+// getProductPrice("Radar");
+// getProductPrice("Grip");
+// getProductPrice("Scanner");
+// getProductPrice("Droid");
+// getProductPrice("Engine");
+
+// home work #19
+
+// const products = [
+//   { name: "Radar", price: 1300, quantity: 4 },
+//   { name: "Scanner", price: 2700, quantity: 3 },
+//   { name: "Droid", price: 400, quantity: 7 },
+//   { name: "Grip", price: 1200, quantity: 9 },
+// ];
+
+// function getAllPropValues(propName) {
+//   // Change code below this line
+//   const result = [];
+//   for (const product of products) {
+//     if (product.hasOwnProperty(propName)) result.push(product[propName]);
+//   }
+//   return result;
+
+//   // Change code above this line
+// }
+
+// getAllPropValues("name");
+// getAllPropValues("quantity");
+// getAllPropValues("price");
+// getAllPropValues("category");
