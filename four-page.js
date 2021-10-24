759594601015620ae3f125611b1f22132d75ae95
BohdanDehodiuk <86683356+BohdanDehodiuk@users.noreply.github.com>
2021-10-24 19:37:38 +0300

    add 4 block

---
+++ four-page.js
@@ -0,0 +1,206 @@
+"use strict";
+
+// home work 1
+
+// function greet(name) {
+//   return `Добро пожаловать ${name}.`;
+// }
+
+// // Вызываем функцию greet
+// // и выводим результат в консоль
+// console.log(greet("Mango"));
+// // Добро пожаловать Mango.
+
+// // Выводим функцию greet
+// // в консоль не вызывая её
+// console.log(greet);
+// /*
+// ƒ greet() {
+//      return `Добро пожаловать ${name}.`;
+// }
+// */
+
+// function makePizza() {
+//   return "Your pizza is being prepared, please wait.";
+// }
+// // Change code below this line
+
+// const result = makePizza();
+// const pointer = makePizza;
+// console.log(result);
+// console.log(pointer);
+
+// home work 2
+
+// Колбэк-функция
+// function greet(name) {
+//   //   consle.log(`Добро пожаловать ${name}.`);
+//   //   return `Добро пожаловать ${name}.`;
+// }
+
+// // // // Функция высшего порядка
+// function registerGuest(name, callback) {
+//   console.log(`Регистрируем гостя ${name}.`);
+//   callback(name);
+// }
+
+// registerGuest("Mango", greet);
+// console.log(registerGuest("Mango", greet));
+// console.log(registerGuest("Mango", greet));
+
+// function deliverPizza(pizzaName) {
+//   return `Delivering ${pizzaName} pizza.`;
+// }
+
+// function makePizza(pizzaName) {
+//   return `Pizza ${pizzaName} is being prepared, please wait...`;
+// }
+
+// // Chande code below this line
+// function makeMessage(pizzaName, callback) {
+//   return callback(pizzaName);
+// }
+
+// makeMessage("Royal Grand", makePizza);
+// console.log(makeMessage("Royal Grand", makePizza));
+
+// makeMessage("Ultracheese", deliverPizza);
+// console.log(makeMessage("Ultracheese", deliverPizza));
+
+// home work 3
+// function registerGuest(name, callback) {
+//   console.log(`Регистрируем гостя ${name}.`);
+//   callback(name);
+// }
+
+// // Передаём инлайн функцию greet как колбэк
+// registerGuest("Mango", function greet(name) {
+//   consle.log(`Добро пожаловать ${name}.`);
+// });
+
+// Передаём инлайн функцию notify как колбэк
+// registerGuest("Poly", function notify(name) {
+//   consle.log(`Уважаемый(ая) ${name}, ваш номер будет готов через 30 минут.`);
+// });
+
+// function makePizza(pizzaName, callback) {
+//   console.log(`Pizza ${pizzaName} is being prepared, please wait...`);
+//   callback(pizzaName);
+// }
+
+// makePizza("Royal Grand", function deliverPizza(pizzaName) {
+//   console.log(`Delivering pizza ${pizzaName}.`);
+// });
+// // Change code below this line
+
+// makePizza("Ultracheese", function eatPizza(pizzaName) {
+//   console.log(`Eating pizza ${pizzaName}.`);
+// });
+
+// home work 4
+// function processCall(recipient) {
+//   // Имитируем доступность абонента случайным числом
+//   const isRecipientAvailable = Math.random() > 0.5;
+//   console.log(isRecipientAvailable);
+
+//   if (!isRecipientAvailable) {
+//     console.log(`Абонент ${recipient} недоступен, оставьте сообщение.`);
+//     // Логика активации автоответчика
+//   } else {
+//     console.log(`Соединяем с ${recipient}, ожидайте...`);
+//     // Логика принятия звонка
+//   }
+// }
+
+// processCall("Mango");
+
+function processCall(recipient, onAvailable, onNotAvailable) {
+  // Имитируем доступеность абонента случайным числом
+  const isRecipientAvailable = Math.random() > 0.5;
+
+  if (!isRecipientAvailable) {
+    onNotAvailable(recipient);
+    return;
+  }
+
+  onAvailable(recipient);
+}
+
+function takeCall(name) {
+  console.log(`Соединяем с ${name}, ожидайте...`);
+  // Логика принятия звонка
+}
+
+function activateAnsweringMachine(name) {
+  console.log(`Абонент ${name} недоступен, оставьте сообщение.`);
+  // Логика активации автоответчика
+}
+
+function leaveHoloMessage(name) {
+  console.log(`Абонент ${name} недоступен, записываем голограмму.`);
+  // Логика записи голограммы
+}
+
+// processCall("Mango", takeCall, activateAnsweringMachine);
+// processCall("Poly", takeCall, leaveHoloMessage);
+
+// const pizzaPalace = {
+//   pizzas: ["Ultracheese", "Smoked", "Four meats"],
+//   order(pizzaName, onSuccess, onError) {
+//     if (this.pizzas.includes(pizzaName)) {
+//       return onSuccess(pizzaName);
+//     }
+//     return onError(
+//       `There is no pizza with a name ${pizzaName} in the assortment.`
+//     );
+//   },
+// };
+// // Change code above this line
+
+// // Callback for onSuccess
+// function makePizza(pizzaName) {
+//   return `Your order is accepted. Cooking pizza ${pizzaName}.`;
+// }
+
+// // Callback for onError
+// function onOrderError(error) {
+//   return `Error! ${error}`;
+// }
+
+// // Method calls with callbacks
+// pizzaPalace.order("Smoked", makePizza, onOrderError);
+// pizzaPalace.order("Four meats", makePizza, onOrderError);
+// pizzaPalace.order("Big Mike", makePizza, onOrderError);
+// pizzaPalace.order("Vienna", makePizza, onOrderError);
+const pizzaPalace = {
+  pizzas: ["Ultracheese", "Smoked", "Four meats"],
+  order(pizzaName, onSuccess, onError) {
+    if (pizzaPalace.pizzas.includes(pizzaName)) {
+      return onSuccess(pizzaName);
+    }
+    return onError(
+      `There is no pizza with a name ${pizzaName} in the assortment.`
+    );
+  },
+};
+// Change code above this line
+
+// Callback for onSuccess
+function makePizza(pizzaName) {
+  return `Your order is accepted. Cooking pizza ${pizzaName}.`;
+}
+
+// Callback for onError
+function onOrderError(error) {
+  return `Error! ${error}`;
+}
+
+// Method calls with callbacks
+pizzaPalace.order("Smoked", makePizza, onOrderError);
+console.log(pizzaPalace.order("Smoked", makePizza, onOrderError));
+pizzaPalace.order("Four meats", makePizza, onOrderError);
+console.log(pizzaPalace.order("Four meats", makePizza, onOrderError));
+pizzaPalace.order("Big Mike", makePizza, onOrderError);
+console.log(pizzaPalace.order("Big Mike", makePizza, onOrderError));
+pizzaPalace.order("Vienna", makePizza, onOrderError);
+console.log(pizzaPalace.order("Vienna", makePizza, onOrderError));
