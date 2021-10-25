b99791cad2f844028e303e3603d3f8ccf015da7d
BohdanDehodiuk <86683356+BohdanDehodiuk@users.noreply.github.com>
2021-10-25 19:38:33 +0300

    add 2 block 4

--- four-page.js
+++ four-page.js
@@ -172,35 +172,471 @@ function leaveHoloMessage(name) {
 // pizzaPalace.order("Four meats", makePizza, onOrderError);
 // pizzaPalace.order("Big Mike", makePizza, onOrderError);
 // pizzaPalace.order("Vienna", makePizza, onOrderError);
-const pizzaPalace = {
-  pizzas: ["Ultracheese", "Smoked", "Four meats"],
-  order(pizzaName, onSuccess, onError) {
-    if (pizzaPalace.pizzas.includes(pizzaName)) {
-      return onSuccess(pizzaName);
-    }
-    return onError(
-      `There is no pizza with a name ${pizzaName} in the assortment.`
-    );
+// const pizzaPalace = {
+//   pizzas: ["Ultracheese", "Smoked", "Four meats"],
+//   order(pizzaName, onSuccess, onError) {
+//     if (pizzaPalace.pizzas.includes(pizzaName)) {
+//       return onSuccess(pizzaName);
+//     }
+//     return onError(
+//       `There is no pizza with a name ${pizzaName} in the assortment.`
+//     );
+//   },
+// };
+// // Change code above this line
+
+// // Callback for onSuccess
+// function makePizza(pizzaName) {
+//   return `Your order is accepted. Cooking pizza ${pizzaName}.`;
+// }
+
+// // Callback for onError
+// function onOrderError(error) {
+//   return `Error! ${error}`;
+// }
+
+// // Method calls with callbacks
+// pizzaPalace.order("Smoked", makePizza, onOrderError);
+// console.log(pizzaPalace.order("Smoked", makePizza, onOrderError));
+// pizzaPalace.order("Four meats", makePizza, onOrderError);
+// console.log(pizzaPalace.order("Four meats", makePizza, onOrderError));
+// pizzaPalace.order("Big Mike", makePizza, onOrderError);
+// console.log(pizzaPalace.order("Big Mike", makePizza, onOrderError));
+// pizzaPalace.order("Vienna", makePizza, onOrderError);
+// console.log(pizzaPalace.order("Vienna", makePizza, onOrderError));
+
+// home work 5
+// const numbers = [5, 10, 15, 20, 25];
+
+// // Классический for
+// for (let i = 0; i < numbers.length; i += 1) {
+//   // console.log(`Индекс ${i}, значение ${numbers[i]}`);
+// }
+
+// // Перебирающий forEach
+// numbers.forEach(function (number, index) {
+//   console.log(`Индекс ${index}, значение ${number}`);
+// });
+
+// function calculateTotalPrice(orderedItems) {
+//   let totalPrice = 0;
+//   // Change code below this line
+
+//   // for (let i = 0; i < orderedItems.length; i += 1) {
+//   //   totalPrice += orderedItems[i];
+//   // }
+//   orderedItems.forEach(function (number) {
+//     totalPrice += number;
+//   });
+
+//   // Change code above this line
+//   return totalPrice;
+// }
+
+// calculateTotalPrice([12, 85, 37, 4]);
+// console.log(calculateTotalPrice([12, 85, 37, 4]));
+// calculateTotalPrice([164, 48, 291]);
+// calculateTotalPrice([412, 371, 94, 63, 176]);
+
+// home work 6
+
+// function filterArray(numbers, value) {
+//   const filteredNumbers = [];
+//   // Change code below this line
+
+//   // for (let i = 0; i < numbers.length; i += 1) {
+//   //   if (numbers[i] > value) {
+//   //     filteredNumbers.push(numbers[i]);
+//   //   }
+//   // }
+//   numbers.forEach(function (number) {
+//     if (number > value) {
+//       filteredNumbers.push(number);
+//     }
+//   });
+
+//   // Change code above this line
+//   return filteredNumbers;
+// }
+// filterArray([1, 2, 3, 4, 5], 3);
+
+// filterArray([1, 2, 3, 4, 5], 4);
+// filterArray([1, 2, 3, 4, 5], 5);
+// filterArray([12, 24, 8, 41, 76], 38);
+// filterArray([12, 24, 8, 41, 76], 20);
+// console.log(filterArray([12, 24, 8, 41, 76], 20));
+
+// home work 7
+// function getCommonElements(firstArray, secondArray) {
+//   const commonElements = [];
+//   // Change code below this line
+
+//   // for (let i = 0; i < firstArray.length; i += 1) {
+//   //   if (secondArray.includes(firstArray[i])) {
+//   //     commonElements.push(firstArray[i]);
+//   //   }
+//   // }
+//   firstArray.forEach((el) => {
+//     if (secondArray.includes(el)) commonElements.push(el);
+//   });
+
+//   return commonElements;
+//   // Change code above this line
+// }
+// getCommonElements([1, 2, 3], [2, 4]);
+// console.log(getCommonElements([1, 2, 3], [2, 4]));
+
+// home work 10
+// Change code below this line
+// const calculateTotalPrice = (orderedItems) => {
+//   let totalPrice = 0;
+
+//   orderedItems.forEach((item) => {
+//     totalPrice += item;
+//   });
+
+//   return totalPrice;
+// };
+// // Change code above this line
+
+// calculateTotalPrice([12, 85, 37, 4]);
+// console.log(calculateTotalPrice([12, 85, 37, 4]));
+// calculateTotalPrice([164, 48, 291]);
+// console.log(calculateTotalPrice([164, 48, 291]));
+// calculateTotalPrice([412, 371, 94, 63, 176]);
+// console.log(calculateTotalPrice([412, 371, 94, 63, 176]));
+
+// home work 11
+// Change code below this line
+// const filterArray = (numbers, value) => {
+//   const filteredNumbers = [];
+
+//   numbers.forEach((number) => {
+//     if (number > value) {
+//       filteredNumbers.push(number);
+//     }
+//   });
+
+//   // Change code above this line
+//   return filteredNumbers;
+// };
+
+// filterArray([1, 2, 3, 4, 5], 3);
+// console.log(filterArray([1, 2, 3, 4, 5], 3));
+// filterArray([1, 2, 3, 4, 5], 4);
+// filterArray([1, 2, 3, 4, 5], 5);
+// filterArray([12, 24, 8, 41, 76], 38);
+// filterArray([12, 24, 8, 41, 76], 20);
+
+// home work 12
+
+// Change code below this line
+// const getCommonElements = (firstArray, secondArray) => {
+//   const commonElements = [];
+
+//   firstArray.forEach((element) => {
+//     if (secondArray.includes(element)) {
+//       commonElements.push(element);
+//     }
+//   });
+
+//   // Change code above this line
+//   return commonElements;
+// };
+// getCommonElements([10, 20, 30, 40], [4, 30, 17, 10, 40]);
+// console.log(getCommonElements([10, 20, 30, 40], [4, 30, 17, 10, 40]));
+// const numbers = [1, 2, 3, 4, 5];
+// const dirtyMultiply = (array, value) => {
+//   for (let i = 0; i < array.length; i += 1) {
+//     array[i] = array[i] * value;
+//   }
+// };
+
+// dirtyMultiply(numbers, 2);
+// // Произошла мутация исходных данных - массива numbers
+// console.log(numbers); // [2, 4, 6, 8, 10]
+
+// home work #13
+// const changeEven = (numbers, value) => {
+//   // Change code below this line
+//   const newArrey = [];
+// for (let i = 0; i < numbers.length; i += 1) {
+//   if (numbers[i] % 2 === 0) {
+//     numbers[i] = numbers[i] + value;
+//     // console.log(numbers[i]);
+//   }
+// }
+//   numbers.forEach((number) => {
+//     if (number % 2 === 0) {
+//       // console.log(number);
+//       newArrey.push(number + value);
+//       // newArrey = number + value;
+
+//       // console.log(newArrey);
+//     } else {
+//       newArrey.push(number);
+//     }
+//   });
+//   return newArrey;
+
+//   // Change code above this line
+// };
+// changeEven([1, 2, 3, 4, 5], 10);
+// console.log(changeEven([1, 2, 3, 4, 5], 10));
+
+// changeEven([17, 24, 68, 31, 42], 100);
+// console.log(changeEven([17, 24, 68, 31, 42], 100));
+
+// home work 24
+// const users = [
+//   {
+//     name: "Moore Hensley",
+//     email: "moorehensley@indexia.com",
+//     eyeColor: "blue",
+//     friends: ["Sharron Pace"],
+//     isActive: false,
+//     balance: 2811,
+//     gender: "male",
+//     age: 37,
+//   },
+//   {
+//     name: "Sharlene Bush",
+//     email: "sharlenebush@tubesys.com",
+//     eyeColor: "blue",
+//     friends: ["Briana Decker", "Sharron Pace"],
+//     isActive: true,
+//     balance: 3821,
+//     gender: "female",
+//     age: 34,
+//   },
+//   {
+//     name: "Ross Vazquez",
+//     email: "rossvazquez@xinware.com",
+//     eyeColor: "green",
+//     friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"],
+//     isActive: false,
+//     balance: 3793,
+//     gender: "male",
+//     age: 24,
+//   },
+//   {
+//     name: "Elma Head",
+//     email: "elmahead@omatom.com",
+//     eyeColor: "green",
+//     friends: ["Goldie Gentry", "Aisha Tran"],
+//     isActive: true,
+//     balance: 2278,
+//     gender: "female",
+//     age: 21,
+//   },
+//   {
+//     name: "Carey Barr",
+//     email: "careybarr@nurali.com",
+//     eyeColor: "blue",
+//     friends: ["Jordan Sampson", "Eddie Strong"],
+//     isActive: true,
+//     balance: 3951,
+//     gender: "male",
+//     age: 27,
+//   },
+//   {
+//     name: "Blackburn Dotson",
+//     email: "blackburndotson@furnigeer.com",
+//     eyeColor: "brown",
+//     friends: ["Jacklyn Lucas", "Linda Chapman"],
+//     isActive: false,
+//     balance: 1498,
+//     gender: "male",
+//     age: 38,
+//   },
+//   {
+//     name: "Sheree Anthony",
+//     email: "shereeanthony@kog.com",
+//     eyeColor: "brown",
+//     friends: ["Goldie Gentry", "Briana Decker"],
+//     isActive: true,
+//     balance: 2764,
+//     gender: "female",
+//     age: 39,
+//   },
+// ];
+
+// const getUsersWithFriend = (users, friendName) => {
+//   return users.filter((user) => user.friends.includes(friendName));
+// };
+
+// getUsersWithFriend(users, "Briana Decker");
+// console.log(getUsersWithFriend(users, "Briana Decker"));
+// getUsersWithFriend(users, "Goldie Gentry");
+// getUsersWithFriend(users, "Adrian Cross");
+
+// // home work 26
+// const users = [
+//   {
+//     name: "Moore Hensley",
+//     email: "moorehensley@indexia.com",
+//     eyeColor: "blue",
+//     friends: ["Sharron Pace"],
+//     isActive: false,
+//     balance: 2811,
+//     gender: "male",
+//     age: 37,
+//   },
+//   {
+//     name: "Sharlene Bush",
+//     email: "sharlenebush@tubesys.com",
+//     eyeColor: "blue",
+//     friends: ["Briana Decker", "Sharron Pace"],
+//     isActive: true,
+//     balance: 3821,
+//     gender: "female",
+//     age: 34,
+//   },
+//   {
+//     name: "Ross Vazquez",
+//     email: "rossvazquez@xinware.com",
+//     eyeColor: "green",
+//     friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"],
+//     isActive: false,
+//     balance: 3793,
+//     gender: "male",
+//     age: 24,
+//   },
+//   {
+//     name: "Elma Head",
+//     email: "elmahead@omatom.com",
+//     eyeColor: "green",
+//     friends: ["Goldie Gentry", "Aisha Tran"],
+//     isActive: true,
+//     balance: 2278,
+//     gender: "female",
+//     age: 21,
+//   },
+//   {
+//     name: "Carey Barr",
+//     email: "careybarr@nurali.com",
+//     eyeColor: "blue",
+//     friends: ["Jordan Sampson", "Eddie Strong"],
+//     isActive: true,
+//     balance: 3951,
+//     gender: "male",
+//     age: 27,
+//   },
+//   {
+//     name: "Blackburn Dotson",
+//     email: "blackburndotson@furnigeer.com",
+//     eyeColor: "brown",
+//     friends: ["Jacklyn Lucas", "Linda Chapman"],
+//     isActive: false,
+//     balance: 1498,
+//     gender: "male",
+//     age: 38,
+//   },
+//   {
+//     name: "Sheree Anthony",
+//     email: "shereeanthony@kog.com",
+//     eyeColor: "brown",
+//     friends: ["Goldie Gentry", "Briana Decker"],
+//     isActive: true,
+//     balance: 2764,
+//     gender: "female",
+//     age: 39,
+//   },
+// ];
+
+// // Change code below this line
+// const getFriends = (users) => {
+//   return users.filter((user) => user.isActive);
+
+//   // return allActivUser;
+// };
+// // Change code above this line
+
+// getFriends(users);
+// console.log(getFriends(users));
+
+// home work 31
+const users = [
+  {
+    name: "Moore Hensley",
+    email: "moorehensley@indexia.com",
+    eyeColor: "blue",
+    friends: ["Sharron Pace"],
+    isActive: false,
+    balance: 2811,
+    gender: "male",
+    age: 37,
   },
+  {
+    name: "Sharlene Bush",
+    email: "sharlenebush@tubesys.com",
+    eyeColor: "blue",
+    friends: ["Briana Decker", "Sharron Pace"],
+    isActive: true,
+    balance: 3821,
+    gender: "female",
+    age: 34,
+  },
+  {
+    name: "Ross Vazquez",
+    email: "rossvazquez@xinware.com",
+    eyeColor: "green",
+    friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"],
+    isActive: false,
+    balance: 3793,
+    gender: "male",
+    age: 24,
+  },
+  {
+    name: "Elma Head",
+    email: "elmahead@omatom.com",
+    eyeColor: "green",
+    friends: ["Goldie Gentry", "Aisha Tran"],
+    isActive: true,
+    balance: 2278,
+    gender: "female",
+    age: 21,
+  },
+  {
+    name: "Carey Barr",
+    email: "careybarr@nurali.com",
+    eyeColor: "blue",
+    friends: ["Jordan Sampson", "Eddie Strong"],
+    isActive: true,
+    balance: 3951,
+    gender: "male",
+    age: 27,
+  },
+  {
+    name: "Blackburn Dotson",
+    email: "blackburndotson@furnigeer.com",
+    eyeColor: "brown",
+    friends: ["Jacklyn Lucas", "Linda Chapman"],
+    isActive: false,
+    balance: 1498,
+    gender: "male",
+    age: 38,
+  },
+  {
+    name: "Sheree Anthony",
+    email: "shereeanthony@kog.com",
+    eyeColor: "brown",
+    friends: ["Goldie Gentry", "Briana Decker"],
+    isActive: true,
+    balance: 2764,
+    gender: "female",
+    age: 39,
+  },
+];
+
+// Change code below this line
+const getFriends = (users) => {
+  return users.every((user) => user.isActive);
+  // console.log(activ);
+
+  // return allActivUser;
 };
 // Change code above this line
 
-// Callback for onSuccess
-function makePizza(pizzaName) {
-  return `Your order is accepted. Cooking pizza ${pizzaName}.`;
-}
-
-// Callback for onError
-function onOrderError(error) {
-  return `Error! ${error}`;
-}
-
-// Method calls with callbacks
-pizzaPalace.order("Smoked", makePizza, onOrderError);
-console.log(pizzaPalace.order("Smoked", makePizza, onOrderError));
-pizzaPalace.order("Four meats", makePizza, onOrderError);
-console.log(pizzaPalace.order("Four meats", makePizza, onOrderError));
-pizzaPalace.order("Big Mike", makePizza, onOrderError);
-console.log(pizzaPalace.order("Big Mike", makePizza, onOrderError));
-pizzaPalace.order("Vienna", makePizza, onOrderError);
-console.log(pizzaPalace.order("Vienna", makePizza, onOrderError));
+getFriends(users);
+console.log(getFriends(users));
